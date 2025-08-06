eaa46828a5f3492fdff6f1c56771cbc1a81bf061
kayvilamarcela <143215615+kayvilamarcela@users.noreply.github.com>
2025-08-06 11:40:29 -0300

    Delete atividades/Atividade-07 directory

--- atividades/Atividade-07/reacts-rotasAtividade/reacts-rotasAtividade/react-rotas/src/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css'
-import * as ReactDOM from "react-dom/client";
-import * as React from "react";
-import App from "./views/App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
-);
